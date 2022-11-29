25411be0399287e9ef7e5b2bc1e529a0266df3f2
Scott McKenzie <scott@noizyland.net>
2022-11-30 08:32:03 +1100

    Fix products-get

--- api/products-get/index.js
+++ api/products-get/index.js
@@ -1,13 +1,11 @@
-module.exports = async function (context, req) {
-    const data = require('../shared/product-data');
+const data = require('../shared/product-data');
 
-    module.exports = async function (context, req) {
-        try {
-            const products = data.getProducts();
-            context.res.status(200).json(products);
-        }
-        catch (error) {
-            context.res.status(500).send(error);
-        }
+module.exports = async function (context, req) {
+    try {
+        const products = data.getProducts();
+        context.res.status(200).json(products);
+    }
+    catch (error) {
+        context.res.status(500).send(error);
     }
 }
